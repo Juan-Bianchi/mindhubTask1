19ea9952c75b2511b25ae1a7e80508f0e2085268
Juan-Bianchi <juanbianchi@icloud.com>
2022-12-27 23:00:08 -0300

    creado main.js se hacen funciones para filtrar los eventos

---
+++ assets/datos/main.js
@@ -0,0 +1,57 @@
+const datos = Object.assign({}, data);;
+
+function createCard (carta) {
+    return (`<article class="card card-medium col-10 col-lg-3 col-md-5 justify-content-center ">
+                <img ${carta.image} class="card-img-top" alt="avengers" title="avengers">
+                <div class="card-body">
+                    <h5 class="card-title">${carta.name}</h5>
+                    <p class="card-text">${carta.description}</p>
+                    <div>
+                        <p class="card-text">Price: $${carta.price}</p>
+                        <a href="./details.html" class="btn btn-outline-light btn-info">See More</a>
+                    </div>
+                </div>
+            </article>`);
+}
+
+
+function cargaTodosLosEventos (listaEventos) {
+   
+    let listaTerminada = []
+    for(evento of listaEventos) {
+        listaTerminada.push(createCard(evento));
+    }
+    return listaTerminada;
+}
+
+function filtraEventosPasados (listaEventos, fechaReferencia) {
+    let listaTerminada = [];
+
+    for(evento of listaEventos) {
+        if(fechaReferencia < evento.date ) {
+            listaTerminada.push(createCard(evento));
+        }
+    }
+    return listaTerminada;
+}
+
+
+function filtraEventosFuturos (listaEventos, fechaReferencia) {
+    let listaTerminada = []
+    for(evento of listaEventos) {
+        if(fechaReferencia >= evento.date ) {
+            listaTerminada.push(createCard(evento));
+        }
+    }
+    return listaTerminada;
+}
+
+let todosLosEventos = cargaTodosLosEventos(datos.events);
+let pastEvents = filtraEventosPasados(datos.events, datos.currentDate);
+let upcomingEvents = filtraEventosFuturos(datos.events, datos.currentDate);
+
+console.log(todosLosEventos);
+console.log(pastEvents);
+console.log(upcomingEvents);
+
+
