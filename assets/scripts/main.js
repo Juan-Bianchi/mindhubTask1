ef69abaede8ef3b00b0d8659083272a465d10287
Juan-Bianchi <juanbianchi@icloud.com>
2023-01-04 01:26:04 -0300

    creacion de checkbox dinamica en Home

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,40 @@
-const datos = Object.assign({}, data);
+const completeData = Object.assign({}, data);
 const cardContainer = document.querySelector(".card-container");
+const checksContainer = document.querySelector(".checks")
+const searchContainer = document.getElementById("search");
+const allEvents = [];
+
+
+//CREO LOS CHECKBOX CON LAS CATEGORIAS
+
+function createCategoryList ( events) {
+    const checkBoxList = events.map( event => event.category)
+                               .filter( ( categ, index, array) => array.indexOf(categ) === index);
+    return checkBoxList;
+}
+
+
+function renderizeChecks ( events, createList,  where) {
+    const checkBoxList = createList(events);
+    
+    checkBoxList.forEach(categ => {
+        where.innerHTML += `<input type="checkbox" class="btn-check" id="${categ}">
+                            <label class="btn btn-fondo-blanco" for="${categ}">${categ}</label>`;
+        
+    });
+}
+
+renderizeChecks(completeData.events, createCategoryList, checksContainer);
+
+//FILTRO SEARCH INPUT
+
+searchContainer.addEventListener('input', e => { 
+
+});
+
+
+
+
 
 function createCard (card, container) {
     container.innerHTML +=`<article class="card card-medium col-10 col-lg-3 col-md-5 justify-content-center ">
@@ -16,14 +51,12 @@ function createCard (card, container) {
 }
 
 
-function loadAllEvents (listaEventos, container) {
-   
-    for(let event of listaEventos.events) {
-        createCard(event, container);
-    }
-}
 
-loadAllEvents(datos, cardContainer)
+function searchByName (events) {
+    const allEvents = completeData.events.map ( event => event)
+    
+
+}
 
 
 
